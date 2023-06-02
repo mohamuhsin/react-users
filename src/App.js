122bf1a25977dcdb6dcd292c1ac9bfb942bdbeae
mohamuhsin <muhsinohoho@outlook.com>
2023-06-02 16:28:00 +0300

    created function to render the list of users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/User/AddUser";
+import UsersList from "./components/UsersList/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -14,11 +14,9 @@ const AddUser = (props) => {
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
     }
-
     if (+enteredAge < 1) {
       return;
     }
-
     console.log(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
