66129270c2fdcddce7f0156c4ce38a062e40017e
mohamuhsin <muhsinohoho@outlook.com>
2023-06-02 15:59:55 +0300

    validated form inputs

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -10,6 +10,15 @@ const AddUser = (props) => {
 
   const AddUserHandler = (event) => {
     event.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
     console.log(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
