4d9d2390d9fc5cf86a5d75f1c6593ef42a69f26c
mohamuhsin <muhsinohoho@outlook.com>
2023-06-02 17:20:26 +0300

    created Error modal component

---
+++ src/components/ErrorModal/ErrorModal.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Card from "../Card/Card";
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+export default ErrorModal;
