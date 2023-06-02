89a38b69e9d12783d64a2e49504bd1f28868b79a
mohamuhsin <muhsinohoho@outlook.com>
2023-06-02 17:23:50 +0300

    added css classes to the errormodal component

--- src/components/ErrorModal/ErrorModal.js
+++ src/components/ErrorModal/ErrorModal.js
@@ -1,17 +1,18 @@
 import React from "react";
 
 import Card from "../Card/Card";
+import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
   return (
-    <Card>
-      <header>
+    <Card className={classes.modal}>
+      <header className={classes.header}>
         <h2>{props.title}</h2>
       </header>
-      <div>
+      <div className={classes.content}>
         <p>{props.message}</p>
       </div>
-      <footer>
+      <footer className={classes.actions}>
         <Button>Okay</Button>
       </footer>
     </Card>
