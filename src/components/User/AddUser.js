b0d81a436682b14aa7ba6253b3c3e75b4d0f6a76
mohamuhsin <muhsinohoho@outlook.com>
2023-06-02 15:24:36 +0300

    built a custom button that replaced the button in the form

--- src/components/User/AddUser.js
+++ src/components/User/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../Card/Card";
 import classes from "./AddUser.module.css";
+import Button from "../Button/Button";
 
 const AddUser = (props) => {
   const AddUserHandler = (event) => {
@@ -14,7 +15,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age</label>
         <input id="age" type="number" />
-        <button type="sumit">Add User</button>
+        <Button type="sumit"> Add User</Button>
       </form>
     </Card>
   );
